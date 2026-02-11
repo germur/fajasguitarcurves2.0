5ee02928367e04c479d372c62ccb27167552265f
Rosher Mur <rogermur1990@gmail.com>
2026-02-11 07:08:15 -0500

    fix(main): restore HelmetProvider and i18n import to resolve crash

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import { router } from './lib/router';
 import { StoreProvider } from './storefront/hooks/useStoreContext';
-import { GlobalErrorBoundary } from './components/GlobalErrorBoundary'; // Import Error Boundary
+import { GlobalErrorBoundary } from './components/GlobalErrorBoundary';
+import './i18n';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GlobalErrorBoundary>
-      <StoreProvider>
-        <RouterProvider router={router} />
-      </StoreProvider>
+      <HelmetProvider>
+        <StoreProvider>
+          <RouterProvider router={router} />
+        </StoreProvider>
+      </HelmetProvider>
     </GlobalErrorBoundary>
   </StrictMode>
 );
